ccf1bcf57807640153bb00a140d6ad2ba1523e63
aaazlkm <40765910+aaazlkm@users.noreply.github.com>
2022-06-20 09:27:34 +0900

    processing core apiまで

---
+++ processing/lib/src/_core.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/widgets.dart';
+
+class Processing extends StatelessWidget {
+  const Processing({
+    required this.sketch,
+    Key? key,
+  }) : super(key: key);
+
+  final Sketch sketch;
+
+  @override
+  Widget build(BuildContext context) => CustomPaint(
+        size: Size.infinite,
+        painter: _SketchPainter(
+          sketch: sketch,
+        ),
+      );
+}
+
+abstract class Sketch {
+  void setup();
+
+  void draw();
+
+  late final Canvas canvas;
+
+  late final Size size;
+
+  void background({
+    required Color color,
+  }) {
+    final paint = Paint()..color = color;
+    canvas.drawRect(Offset.zero & size, paint);
+  }
+}
+
+class _SketchPainter extends CustomPainter {
+  _SketchPainter({
+    required this.sketch,
+  });
+
+  final Sketch sketch;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    sketch
+      ..canvas = canvas
+      ..size = size
+      ..setup()
+      ..draw();
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}

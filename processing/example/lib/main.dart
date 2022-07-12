cb95511bac033dcd33fcf92c6436e3c8efc7e27c
aaazlkm <40765910+aaazlkm@users.noreply.github.com>
2022-07-12 09:16:41 +0900

    implement start animation

--- processing/example/lib/main.dart
+++ processing/example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:processing/processing.dart';
 
@@ -26,7 +28,13 @@ class Page extends StatefulWidget {
 }
 
 class _PageState extends State<Page> {
-  Offset _circularOffset = Offset.zero;
+  final stars = <Star>[];
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    stars.clear();
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -34,20 +42,106 @@ class _PageState extends State<Page> {
           child: Processing(
             sketch: Sketch.simple(
               setup: (s) {
-                print('setup');
-                s.size(width: 200, height: 200);
+                const width = 800.0;
+                const height = 800.0;
+                s
+                  ..size(width: width, height: height)
+                  ..background(
+                    color: Colors.black,
+                  );
+
+                for (var i = 0; i < 500; i++) {
+                  stars.add(
+                    Star(
+                      x: width / 2 + s.random(-width.toInt(), width.toInt()),
+                      y: s.random(-height.toInt(), height.toInt()),
+                      z: s.random(width.toInt()),
+                    ),
+                  );
+                }
               },
               draw: (s) {
-                print('draw ${s.frameRate}');
+                for (final star in stars) {
+                  star.update(s);
+                }
 
-                s
-                  ..noStroke()
-                  ..background(color: Colors.black)
-                  ..rect(rect: Rect.fromLTWH(40, 0, 20, s.height.toDouble()))
-                  ..rect(rect: Rect.fromLTWH(60, 0, 20, s.height / 2));
+                for (final star in stars) {
+                  star.paintStroke(s);
+                }
+
+                for (final star in stars) {
+                  star.paintStar(s);
+                }
               },
             ),
           ),
         ),
       );
+}
+
+class Star {
+  Star({
+    required this.x,
+    required this.y,
+    required this.z,
+  }) : strokeStartZ = 0 {
+    strokeStartZ = z;
+  }
+
+  double x;
+  double y;
+  double z;
+  double strokeStartZ;
+
+  void update(Sketch s) {
+    z -= 10;
+    strokeStartZ -= 5;
+
+    if (z < 0) {
+      x = s.random(-s.width.toInt(), s.width.toInt());
+      y = s.random(-s.height.toInt(), s.height.toInt());
+      z = s.random(s.width.toInt());
+      strokeStartZ = z;
+    }
+  }
+
+  void paintStroke(Sketch s) {
+    final center = Offset(
+      s.width / 2,
+      s.height / 2,
+    );
+    final perspectiveOffsetStart = Offset(
+      lerpDouble(0, s.width, x / strokeStartZ)!,
+      lerpDouble(0, s.height, y / strokeStartZ)!,
+    );
+    final perspectiveOffset = Offset(
+      lerpDouble(0, s.width, x / z)!,
+      lerpDouble(0, s.height, y / z)!,
+    );
+    s
+      ..stroke(color: Colors.white.withOpacity(0.3))
+      ..line(perspectiveOffsetStart + center, perspectiveOffset + center);
+  }
+
+  void paintStar(Sketch s) {
+    final center = Offset(
+      s.width / 2,
+      s.height / 2,
+    );
+    final perspectiveOffset = Offset(
+      lerpDouble(0, s.width, x / z)!,
+      lerpDouble(0, s.height, y / z)!,
+    );
+    final diameter = lerpDouble(12, 0, z / s.width)!;
+
+    s
+      ..noStroke()
+      ..fill(
+        color: Colors.white,
+      )
+      ..circle(
+        center: center + perspectiveOffset,
+        diameter: diameter,
+      );
+  }
 }

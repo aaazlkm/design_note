c6c59e704f57b5a248cd04988cb7f5a1f4c8c8f9
aaazlkm <40765910+aaazlkm@users.noreply.github.com>
2022-08-04 09:38:07 +0900

    droidkaigi favorite button composeの勉強

--- design_note_android/app/src/main/java/com/example/design_note_android/MainActivity.kt
+++ design_note_android/app/src/main/java/com/example/design_note_android/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.design_note_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.design_note_android.favoritebutton.FavoriteButtonScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MaterialTheme {
+                AppContent()
+            }
+        }
+    }
+}
+
+sealed class AppRoutes {
+    object Home : AppRoutes()
+
+    val path: String
+        get() = when (this) {
+            Home -> "home"
+        }
+}
+
+@Composable
+fun AppContent() {
+    val navController = rememberNavController()
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = AppRoutes.Home.path,
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            composable(AppRoutes.Home.path) {
+                FavoriteButtonScreen()
+            }
+        }
     }
 }

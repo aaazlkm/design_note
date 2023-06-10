267db4aa3f26fa61c3466d30147ca3c79a4a45b4
aaazlkm <40765910+aaazlkm@users.noreply.github.com>
2023-06-10 19:12:36 +0900

    ライブラリ追加

--- youtube/lib/main.dart
+++ youtube/lib/main.dart
@@ -8,8 +8,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -17,7 +16,6 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -31,8 +29,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  Widget build(BuildContext context) => Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
@@ -48,5 +45,4 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
-  }
 }
